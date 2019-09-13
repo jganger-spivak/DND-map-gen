5d3f788ccda2bfdef78e9c953a3410c79fe6e115
ijensen <ijensen@techvalleyhigh.org>
2019-09-13 09:35:09 -0400

    setup in intellij added first file set up some variables for later this is where the fun begins

---
+++ src/MapGen.java
@@ -0,0 +1,16 @@
+public class MapGen {
+
+    public static void main(String[] args){
+
+        // Max size maps can be generated within
+        int maxMapSizeX;
+        int maxMapSizeY;
+        int maxMapSizeZ;
+
+        System.out.println("this is where the fun begins");
+
+
+
+    }
+
+}
