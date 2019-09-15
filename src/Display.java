9a593ab697bc749cb1f9b6d0302f9e90b7050824
ijensen1 <ijensen@techvalleyhigh.org>
2019-09-14 22:23:08 -0400

    added a button that expands white rectangle just general playing around to learn JPanel

--- src/Display.java
+++ src/Display.java
@@ -1,11 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Display extends JPanel {
+
+    private int width;
+    private int height;
 
     public Display(){
         setLayout(null);
         repaint();
+        width = 100;
+        height = 100;
+        JButton button = new JButton("Expand");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                width = 200;
+                height = 200;
+                repaint();
+            }
+        });
+        button.setBounds(400, 100, 120, 30);
+        add(button);
     }
 
     public void paintComponent(Graphics g){
@@ -13,7 +31,7 @@ public class Display extends JPanel {
         super.paintComponent(g);
         setBackground(Color.BLACK);
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 100, 100);
+        g.fillRect(0, 0, width, height);
 
     }
 
