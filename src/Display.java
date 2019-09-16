ffaaf2c6229f609c86c03731c5cce0a6784f758e
ijensen <ijensen@techvalleyhigh.org>
2019-09-16 09:28:40 -0400

    ignore Rooms Class Display Class additions: - small white cube can now be moved around on screen using the onscreen arrow keys. this was a small test for placing a "room" where we want on screen. this is to be evolved

--- src/Display.java
+++ src/Display.java
@@ -7,23 +7,56 @@ public class Display extends JPanel {
 
     private int width;
     private int height;
+    private int xPos;
+    private int yPos;
 
     public Display(){
         setLayout(null);
         repaint();
-        width = 100;
-        height = 100;
-        JButton button = new JButton("Expand");
-        button.addActionListener(new ActionListener() {
+        width = 25;
+        height = 25;
+        xPos = 0;
+        yPos = 300;
+        JButton upButton = new JButton("^");
+        upButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                width = 200;
-                height = 200;
+                yPos = yPos - 50;
                 repaint();
             }
         });
-        button.setBounds(400, 100, 120, 30);
-        add(button);
+        upButton.setBounds(400, 100, 50, 50);
+        add(upButton);
+        JButton downButton = new JButton("v");
+        downButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                yPos = yPos + 50;
+                repaint();
+            }
+        });
+        downButton.setBounds(400, 200, 50, 50);
+        add(downButton);
+        JButton rightButton = new JButton(">");
+        rightButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                xPos = xPos + 50;
+                repaint();
+            }
+        });
+        rightButton.setBounds(450, 150, 50, 50);
+        add(rightButton);
+        JButton leftButton = new JButton("<");
+        leftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                xPos = xPos - 50;
+                repaint();
+            }
+        });
+        leftButton.setBounds(350, 150, 50, 50);
+        add(leftButton);
     }
 
     public void paintComponent(Graphics g){
@@ -31,8 +64,10 @@ public class Display extends JPanel {
         super.paintComponent(g);
         setBackground(Color.BLACK);
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, width, height);
+        g.fillRect(xPos, yPos, width, height);
 
     }
 
-}
+
+
+    }
---
+++ src/Rooms.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Rooms extends JPanel {
+
+   /*
+    public static void room(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.WHITE);
+        g.fillRect(xPos, yPos, 50, 50);
+       }
+   */
+
+
+}
